316c5d82066d7bc2ee7ba834b392f9be23bf93ff
lynx0421 <lynx0421@qq.com>
2017-06-21 07:29:59 +0800

    finishes vc partial for ver2

--- src/app/shared/vehicle-details/vehicle-details-costs/dialog-new-vehicle-cost.component.ts
+++ src/app/shared/vehicle-details/vehicle-details-costs/dialog-new-vehicle-cost.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MdDialogRef, MD_DIALOG_DATA } from '@angular/material';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SharedValidatorsService } from '../../validators/shared-validators.service';
 
 @Component({
   selector: 'app-dialog-new-vehicle-cost',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogNewVehicleCostComponent implements OnInit {
 
-  constructor() { }
+  vcForm: FormGroup;
+
+  constructor(
+    public dialogRef: MdDialogRef<DialogNewVehicleCostComponent>,
+    public fb: FormBuilder,
+    @Inject(MD_DIALOG_DATA) public data: any,
+    private sv: SharedValidatorsService
+    ) { }
 
   ngOnInit() {
+    this.vcForm = this.fb.group({
+      type: ['', [
+        Validators.required,
+        this.sv.notListedButCanBeEmpty(this.data.btity.types['vehicleCostTypes'].map(type => type.name))
+      ]],
+      details: [''],
+      amount: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit() {
+    this.vcForm.disable(); // disable the form before adding it into the parent form
+    this.dialogRef.close(this.vcForm);
   }
 
 }
--- src/app/shared/vehicle-details/vehicle-details-costs/vehicle-details-costs.component.ts
+++ src/app/shared/vehicle-details/vehicle-details-costs/vehicle-details-costs.component.ts
@@ -1,26 +1,37 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { AbstractControl, FormArray, FormBuilder, FormGroup, Validators, ValidatorFn, FormControl } from '@angular/forms';
 import { SharedValidatorsService } from '../../validators/shared-validators.service';
 import { FormUtilsService } from '../../form-utils/form-utils.service';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import { MdDialog } from '@angular/material';
+
+import { DialogNewVehicleCostComponent } from './dialog-new-vehicle-cost.component';
+
 
 @Component({
   selector: 'app-vehicle-details-costs',
   templateUrl: './vehicle-details-costs.component.html',
   styleUrls: ['./vehicle-details-costs.component.scss']
 })
-export class VehicleDetailsCostsComponent implements OnInit {
+export class VehicleDetailsCostsComponent implements OnInit, OnDestroy {
   isCollapsed = true;
   valueChangesRx: Observable<any>;
   fform: FormGroup;
   @Input() vehicle: any;
   @Input() btity: any;
   @Input() isPersonRxx: BehaviorSubject<boolean>;
+  costSum: number;
+  hasTBD: boolean = false;
+  subscriptions: Subscription[] = [];
+  @Input() rvAfterFD: number;
+
   constructor(
     private fb: FormBuilder,
     private sv: SharedValidatorsService,
-    private fu: FormUtilsService
+    private fu: FormUtilsService,
+    private dialog: MdDialog
   ) { }
   ngOnInit() {
     this.fform = this.fb.group({
@@ -46,6 +57,28 @@ export class VehicleDetailsCostsComponent implements OnInit {
         }
       });
 
+
+    const sum_ = this.fform.valueChanges
+      .startWith({vehicleCosts: this.vehicle.vehicleCosts})
+      .subscribe(v => {
+        let sumWithoutRV = 0;
+        let prodWithoutRV = 1;
+        const VCs = v.vehicleCosts;
+        if (VCs) {
+          VCs.forEach(vc => {
+            console.log(vc);
+            const amount = vc['amount'];
+            sumWithoutRV += +amount;
+            prodWithoutRV *= +amount;
+          })
+        }
+        this.costSum = sumWithoutRV + this.rvAfterFD;
+        this.hasTBD = !prodWithoutRV;
+      })
+
+    this.subscriptions.push(sum_);
+
+
   }
 
   onYesNoClose(event, index) {
@@ -57,8 +90,23 @@ export class VehicleDetailsCostsComponent implements OnInit {
   }
 
   openDialogNewVehicleCosts() {
-    console.log('open');
+    const dialogRef = this.dialog.open(DialogNewVehicleCostComponent, {
+      data: {btity: this.btity},
+    });
+    dialogRef.afterClosed().subscribe((newVCForm: FormGroup) => {
+      if (newVCForm) {
+        (this.fform.get('vehicleCosts') as FormArray).push(newVCForm);
+        this.fform.markAsTouched();
+        this.fform.markAsDirty();
+      }
+    });
   }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(sub_ => sub_.unsubscribe());
+  }
+
+
 
 
 }
