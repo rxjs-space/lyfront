a3e12d521993182c112a70f11da1de7bf4e32108
lynx0421 <lynx0421@qq.com>
2017-06-26 13:10:25 +0800

    adds vList for clicking on entrance number

--- src/app/vehicles/entrance/entrance-reports/entrance-reports.component.ts
+++ src/app/vehicles/entrance/entrance-reports/entrance-reports.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MdDialog } from '@angular/material';
+
+import { DialogVehicleListComponent } from '../../../shared/dialog-vehicle-list/dialog-vehicle-list.component';
 
 @Component({
   selector: 'app-entrance-reports',
@@ -7,9 +10,39 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class EntranceReportsComponent implements OnInit {
   @Input() reports;
-  constructor() { }
+
+  constructor(
+    public dialog: MdDialog,
+  ) { }
 
   ngOnInit() {
   }
+
+  queryVehicles(vType, date) {
+    const searchQuery = {'vehicle.vehicleType': vType, 'entranceDate': date}
+    const dialogRef = this.dialog.open(DialogVehicleListComponent, {
+      width: '80%',
+      // disableClose: true,
+      data: {
+        searchQuery,
+        source: '车辆入场',
+        vehicleType: vType,
+        entranceDate: date
+        // types: this.zipData.types,
+        // titles: this.zipData.titles,
+        // vin: vehicleBrief.vin,
+        // vehicleType: vehicleBrief.vehicle.vehicleType,
+        // canCreateNew: !vehicleBrief.dismantling && !vehicleBrief.status.dismantled.done
+      },
+    });
+
+    dialogRef.afterClosed().subscribe(v => {
+      // if (v) {this.needUpdate.emit(true); }
+      // this.needUpdate.emit(v);
+    });
+
+
+  }
+
 
 }
